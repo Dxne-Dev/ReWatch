e070d96240fff0dc538539954d7de14c96f40419
Dxne-Dev <alassanemomo244@gmail.com>
2026-07-07 11:44:45 +0100

    Add SVG favicon with RW monogram amber-purple gradient

---
+++ check.js
@@ -0,0 +1,9 @@
+﻿const fs = require("fs");
+const h = fs.readFileSync("index.html", "utf8");
+const m = h.match(/<link rel="icon"[^>]+>/);
+if (m) {
+  console.log("Favicon OK");
+  const href = m[0].match(/href="([^"]+)"/)[1];
+  const svg = decodeURIComponent(href.replace("data:image/svg+xml,", ""));
+  console.log(svg);
+}
